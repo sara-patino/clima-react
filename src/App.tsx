269ad01fa14b2746b4e213e3addbf0b4fc9469c0
sara-patino <sara.patino@alumnos.uneatlantico.es>
2025-08-17 19:18:50 -0600

    avances

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import styles from "./App.module.css"
+import Form from "./components/Form/Form"
+
 
 function App() {
 
   return (
     <>
-      <h1>Welcome to My App</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+          <Form />
+          <p>2</p>
+      </div>
     </>
   )
 }
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from "react";
+import styles from './Alert.module.css'
+
+export default function Alert({children} : {children : ReactNode}) {
+  return (
+    <div className={styles.alert}>{children}</div>
+  )
+}
